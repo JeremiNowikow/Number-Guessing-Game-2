55ac43129d99b9f424fedaec76124ef0b68d687f
JeremiNowikow <jeremi.nowikow@gmail.com>
2023-03-24 16:34:18 +0100

    small variable naming fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # returns a number guessed by the computer in the provided range
-def guess_number(maximum_n:int, minimum_n:int) -> int:
-    return int((max - min) / 2) + min
+def guess_number(maximum_n: int, minimum_n: int) -> int:
+    return int((max_ - min_) / 2) + min_
 
 
 # takes the input of the user and validates it
@@ -16,19 +16,19 @@ def get_user_input() -> str:
 print("Think of a number between 0 and 1000 and I'll guess it.")
 print("Guide me by typing 'too small', 'too big' or 'you win' in the console.")
 
-min = 0
-max = 1000
+min_ = 0
+max_ = 1000
 
 while True:
-    guess = guess_number(min, max)
+    guess = guess_number(min_, max_)
     print(f"I'm guessing: " + str(guess))
     user_input = get_user_input()
-    if (max == min) and (user_input != "you win"):
+    if (max_ == min_) and (user_input != "you win"):
         print("Do not cheat.")
     elif user_input == "too small":
-        min = guess
+        min_ = guess
     elif user_input == "too big":
-        max = guess
+        max_ = guess
     elif user_input == "you win":
         print("Yay, I've won!")
         break
